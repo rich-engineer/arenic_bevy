75740433a20973cb97af43160040a91c4d5b6827
matthew harwood <matthhar12@gmail.com>
2024-12-30 18:29:13 -0800

    Added tiles system

--- src/tiles.rs
+++ src/tiles.rs
@@ -1,29 +1,20 @@
 use bevy::prelude::*;
 
-fn draw_tile(
-    mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<ColorMaterial>>
-) {
-    // Parent white square (19x19)
+fn draw_tile(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn((
-        Mesh2d(meshes.add(Rectangle::new(19.0, 19.0))),
-        MeshMaterial2d(materials.add(Color::hsla(0.0, 0.0, 1.0, 1.0))),
-    ))
-        .with_children(|parent| {
-            // Child black square (1x1)
-            parent.spawn((
-                Mesh2d(meshes.add(Rectangle::new(1.0, 1.0))),
-                MeshMaterial2d(materials.add(Color::hsla(0.0, 0.0, 0.0, 1.0))),
-            ));
-        });
+        Sprite {
+            custom_size: Some(Vec2::new(19.0, 19.0)),
+            image: asset_server.load("default_tile.png"),
+            ..default()
+        },
+        Transform::default(),
+    ));
 }
 
 pub struct TilesPlugin;
 
 impl Plugin for TilesPlugin {
     fn build(&self, app: &mut App) {
-        app
-            .add_systems(Update, draw_tile);
+        app.add_systems(Update, draw_tile);
     }
 }
